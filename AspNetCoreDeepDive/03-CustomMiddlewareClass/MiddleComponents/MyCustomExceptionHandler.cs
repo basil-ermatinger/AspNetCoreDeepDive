768f01a32bd8f57dfab1a45dbba4b67bb56f120c
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-01 08:35:19 +0200

    Example for Error-Handling-Middleware

---
+++ AspNetCoreDeepDive/03-CustomMiddlewareClass/MiddleComponents/MyCustomExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _03_CustomMiddlewareClass.MiddleComponents
+{
+	public class MyCustomExceptionHandler : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				context.Response.ContentType = "text/html";
+				await next(context);
+			}
+			catch (Exception ex) 
+			{
+				await context.Response.WriteAsync("<h2>Error:</h2>");
+				await context.Response.WriteAsync("<h3>Message</h3>");
+				await context.Response.WriteAsync($"<p>{ex.Message}</p>");
+				await context.Response.WriteAsync("<h3>Data</h3>");
+				await context.Response.WriteAsync($"<p>{ex.Data}</p>");
+				await context.Response.WriteAsync("<h3>StackTrace</h3>");
+				await context.Response.WriteAsync($"<p>{ex.StackTrace}</p>");
+			}
+		}
+	}
+}
--- AspNetCoreDeepDive/03-CustomMiddlewareClass/Program.cs
+++ AspNetCoreDeepDive/03-CustomMiddlewareClass/Program.cs
@@ -3,8 +3,11 @@ using _03_CustomMiddlewareClass.MiddleComponents;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<MyCustomMiddleware>();
+builder.Services.AddTransient<MyCustomExceptionHandler>();
 
 WebApplication app = builder.Build();
+
+app.UseMiddleware<MyCustomExceptionHandler>();
 
 // Middleware #1
 app.Use(async (HttpContext context, RequestDelegate next) =>
@@ -17,6 +20,7 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
 	await context.Response.WriteAsync("Middleware #1: After calling next\r\n");
 });
 
+// Middlware #5 and #6
 app.Map("/map", (appBuilder) =>
 {
 	// Middleware #5
@@ -46,6 +50,14 @@ app.UseMiddleware<MyCustomMiddleware>();
 // Middleware #2
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
+	Random random = new Random();
+	int randomNumber = random.Next(1,3);
+
+	if(randomNumber == 2)
+	{
+		throw new ApplicationException("Exception for testing.");
+	}
+
 	await context.Response.WriteAsync("Middleware #2: Before calling next\r\n");
 
 	await next(context);
