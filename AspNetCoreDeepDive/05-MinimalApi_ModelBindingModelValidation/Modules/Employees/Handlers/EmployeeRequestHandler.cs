3ecac559b5c330a14d3d0fabe082d6943af16147
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-11 20:11:48 +0200

    Added a project for chapter "05 Minimal API: Model Binding & Model Validation"

---
+++ AspNetCoreDeepDive/05-MinimalApi_ModelBindingModelValidation/Modules/Employees/Handlers/EmployeeRequestHandler.cs
@@ -0,0 +1,158 @@
+﻿using _05_MinimalApi_ModelBindingModelValidation.Modules.Employees.Models;
+using _05_MinimalApi_ModelBindingModelValidation.Modules.Employees.Repositories;
+using System.Text.Json;
+
+namespace _05_MinimalApi_ModelBindingModelValidation.Modules.Employees.Handlers
+{
+	public static class EmployeeRequestHandler
+	{
+		public static async Task HandlePost(HttpContext context)
+		{
+			using StreamReader reader = new StreamReader(context.Request.Body);
+			string body = await reader.ReadToEndAsync();
+
+			try
+			{
+				Employee employee = JsonSerializer.Deserialize<Employee>(body)!;
+
+				if(employee is null || employee.Id <= 0)
+				{
+					context.Response.StatusCode = 400;
+					return;
+				}
+
+				EmployeeRepository.AddEmployee(employee);
+
+				context.Response.StatusCode = 201;
+				await context.Response.WriteAsync("Employee added successfully");
+			}
+			catch(Exception ex)
+			{
+				context.Response.StatusCode = 400;
+				await context.Response.WriteAsync(ex.ToString());
+				return;
+			}
+		}
+
+		public static async Task HandleGetEmployees(HttpContext context)
+		{
+			context.Response.ContentType = "text/html";
+
+			List<Employee> employees = EmployeeRepository.GetEmployees();
+
+			await context.Response.WriteAsync("<ul>");
+
+			foreach(Employee employee in employees)
+			{
+				await context.Response.WriteAsync(
+					$"<li><b>{employee.Name}</b>: {employee.Position}</li>");
+			}
+
+			await context.Response.WriteAsync("</ul>");
+		}
+
+		public static async Task HandleGetEmployeeById(HttpContext context)
+		{
+			context.Response.ContentType = "text/html";
+
+			int id = int.Parse(context.Request.RouteValues["id"].ToString());
+
+			Employee employee = EmployeeRepository.GetEmployee(id);
+
+			if(employee is not null)
+			{
+				await context.Response.WriteAsync($"Name: {employee.Name}<br/>");
+				await context.Response.WriteAsync($"Position: {employee.Position}<br/>");
+				await context.Response.WriteAsync($"Salary: {employee.Salary}");
+			}
+			else
+			{
+				context.Response.StatusCode = 404;
+				await context.Response.WriteAsync("Employee not found.");
+			}
+		}
+
+		public static async Task HandleGetPositionById(HttpContext context)
+		{
+			context.Response.ContentType = "text/html";
+
+			int id = int.Parse(context.Request.RouteValues["id"].ToString());
+
+			Employee employee = EmployeeRepository.GetEmployee(id);
+
+			if(employee is not null)
+			{
+				await context.Response.WriteAsync($"{employee.Position}<br/>");
+			}
+			else
+			{
+				context.Response.StatusCode = 404;
+				await context.Response.WriteAsync("Employee not found.");
+			}
+		}
+
+		public static async Task HandleGetSalaryById(HttpContext context)
+		{
+			context.Response.ContentType = "text/html";
+
+			int id = int.Parse(context.Request.RouteValues["id"].ToString());
+
+			Employee employee = EmployeeRepository.GetEmployee(id);
+
+			if(employee is not null)
+			{
+				await context.Response.WriteAsync($"{employee.Salary}<br/>");
+			}
+			else
+			{
+				context.Response.StatusCode = 404;
+				await context.Response.WriteAsync("Employee not found.");
+			}
+		}
+
+		public static async Task HandlePut(HttpContext context)
+		{
+			using StreamReader reader = new StreamReader(context.Request.Body);
+			string body = await reader.ReadToEndAsync();
+			Employee employee = JsonSerializer.Deserialize<Employee>(body)!;
+
+			bool result = EmployeeRepository.UpdateEmployee(employee);
+
+			if(result)
+			{
+				context.Response.StatusCode = 200;
+				await context.Response.WriteAsync("Employee updated successfully!");
+				return;
+			}
+			else
+			{
+				await context.Response.WriteAsync("Employee not found");
+			}
+		}
+
+		public static async Task HandleDelete(HttpContext context)
+		{
+			int id = int.Parse(context.Request.RouteValues["id"].ToString());
+
+			if(context.Request.Headers["Authorization"] == "frank")
+			{
+				bool result = EmployeeRepository.DeleteEmployee(id);
+
+				if(result)
+				{
+					await context.Response.WriteAsync("Employee is deleted successfully.");
+				}
+				else
+				{
+					context.Response.StatusCode = 404;
+					await context.Response.WriteAsync("Employee not found.");
+				}
+			}
+			else
+			{
+				context.Response.StatusCode = 401;
+				await context.Response.WriteAsync("You are not authorized to delete.");
+			}
+		}
+	}
+}
---
+++ AspNetCoreDeepDive/05-MinimalApi_ModelBindingModelValidation/Program.cs
@@ -0,0 +1,49 @@
+using _05_MinimalApi_ModelBindingModelValidation.Modules.Employees.Handlers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapPost("/employees", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandlePost(context);
+});
+
+app.MapGet("/employees", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandleGetEmployees(context);
+});
+
+app.MapGet("/employees/{id:int}", (int id) =>
+{
+	//
+});
+
+app.MapGet("/employees/{id:int}", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandleGetEmployeeById(context);
+});
+
+app.MapGet("/employees/position/{id}", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandleGetPositionById(context);
+});
+
+app.MapGet("/employees/salary/{id}", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandleGetSalaryById(context);
+});
+
+app.MapPut("/employees", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandlePut(context);
+});
+
+app.MapDelete("/employees/{id}", async (HttpContext context) =>
+{
+	await EmployeeRequestHandler.HandleDelete(context);
+});
+
+app.Run();
