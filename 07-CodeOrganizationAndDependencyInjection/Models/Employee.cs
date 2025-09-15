2906a39504566eb04eb5627a78175a3ac5bf910b
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-15 10:14:16 +0200

    Created project 07 and reorganized code.

---
+++ 07-CodeOrganizationAndDependencyInjection/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _07_CodeOrganizationAndDependencyInjection.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		[Required]
+		public string Position { get; set; }
+
+		public double Salary { get; set; }
+
+		public Employee(int id, string name, string position, double salary)
+		{
+			Id = id;
+			Name = name;
+			Position = position;
+			Salary = salary;
+		}
+	}
+}
---
+++ 07-CodeOrganizationAndDependencyInjection/Results/HtmlResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace _07_CodeOrganizationAndDependencyInjection.Results
+{
+	public class HtmlResult : IResult
+	{
+		private readonly string _html;
+
+		public HtmlResult(string html)
+		{
+			_html = html;
+		}
+
+		public async Task ExecuteAsync(HttpContext httpContext)
+		{
+			httpContext.Response.ContentType = "text/html";
+			httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
+
+			await httpContext.Response.WriteAsync(_html);
+		}
+
+	}
+}
