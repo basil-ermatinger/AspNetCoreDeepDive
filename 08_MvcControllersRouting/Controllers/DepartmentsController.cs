4fc9107d7d559d80e5c5a44e2e54bc04db12067a
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-10-07 19:40:21 +0200

    Created project for MVC Controller Routing and first class for a simple DeparmentsController.

---
+++ 08_MvcControllersRouting/Controllers/DepartmentsController.cs
@@ -0,0 +1,15 @@
+﻿namespace _08_MvcControllersRouting.Controllers
+{
+	public class DepartmentsController
+	{
+		public string GetDepartments()
+		{
+			return "These are the departments";
+		}
+
+		public string GetDepartmentById(int id)
+		{
+			return $"Department info: {id}";
+		}
+	}
+}
